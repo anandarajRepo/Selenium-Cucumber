a7d9409960f3ce907edd574006f72783ee4ac827
Anandaraj <anandaraj76@gmail.com>
2021-05-21 10:55:11 +0530

    Report Generation

--- src/test/java/Runner/testrunner.java
+++ src/test/java/Runner/testrunner.java
@@ -1,20 +1,22 @@
 package Runner;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
-import com.cucumber.listener.Reporter;
-
 import io.cucumber.junit.CucumberOptions;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 features = "src/test/java/Features",
 glue= {"seleniumgluecode"},
-plugin = { "pretty", "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html" },
+plugin = { "pretty", "json:target/cucumber.json" },
 monochrome = true
 )
 
@@ -22,8 +24,27 @@ public class testrunner {
 	
 	@AfterClass
     public static void writeExtentReport() {
-        Reporter.loadXMLConfig(new File("config/report.xml"));
+//        Reporter.loadXMLConfig(new File("config/report.xml"));
+        
+        File reportOutputDirectory = new File("target");
+        ArrayList<String> jsonFiles = new ArrayList<String>();
+        jsonFiles.add("target/cucumber.json");
+
+        String projectName = "testng-cucumber";
+
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+        configuration.addClassifications("Platform", System.getProperty("os.name"));
+        configuration.addClassifications("Browser", "Chrome");
+        configuration.addClassifications("Branch", "release/1.0");
+
+        // optionally add metadata presented on main page via properties file
+        List<String> classificationFiles = new ArrayList<String>();
+        classificationFiles.add(System.getProperty("user.dir") + "/config/config.properties");
+        configuration.addClassificationFiles(classificationFiles);
+
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+        reportBuilder.generateReports();
     
     }
-
+	
 }
